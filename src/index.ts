2ee2fd02e5977332c4426b128be6b78e385c0efa
codecomfort <hiroyuki.naitou+codecomfort@gmail.com>
2018-10-27 03:10:11 +0900

    toggle button

--- src/index.ts
+++ src/index.ts
@@ -2,8 +2,27 @@ import "@babel/polyfill";
 import * as Comlink from "comlinkjs";
 import { MarkdownCompiler } from "./markdownWorker";
 
-const editor = document.querySelector(".js-editor");
-const preview = document.querySelector(".js-preview");
+const querySelector = <T>(selectors: string): T => {
+  const element = document.querySelector(selectors);
+  if (!element) {
+    throw new Error(`No ${selectors} found`);
+  }
+
+  // Type Assertion · TypeScript Deep Dive
+  // https://basarat.gitbooks.io/typescript/docs/types/type-assertion.html
+  const typed = (element as any) as T;
+  if (!typed) {
+    // セレクター違いはもちろん、適切に型キャストされない場合も例外を飛ばす
+    // →キャストはランタイムチェックであり、TS は静的チェックしか行えない
+    // なので、例えば button のつもりで div 要素を取得していても気づくのが遅れるため
+    throw new Error(`Invalid type`);
+  }
+  return typed;
+};
+const editor = querySelector<HTMLInputElement>(".js-editor");
+const preview = querySelector<HTMLElement>(".js-preview");
+const previewContainer = querySelector<HTMLElement>(".js-preview-container");
+const toggle = querySelector<HTMLElement>(".js-preview-toggle");
 const worker = new Worker("./markdownWorker.ts");
 const MdCompiler = Comlink.proxy(worker) as new () => MarkdownCompiler;
 
@@ -27,11 +46,30 @@ const main = async () => {
     });
   };
 
-  if (editor && preview) {
-    editor.addEventListener("compositionstart", async (event) => {
+  if (editor && preview && previewContainer && toggle) {
+    // ToggleButton
+    const SHOW_PREVIEW_KEY = "show-preview";
+    const val = window.localStorage.getItem(SHOW_PREVIEW_KEY);
+    // How can I convert a string to boolean in JavaScript? - Stack Overflow
+    // https://stackoverflow.com/questions/263965/how-can-i-convert-a-string-to-boolean-in-javascript
+    let showPreview = val ? val.toLocaleLowerCase() === "true" : true;
+
+    const updatePreviewContainer = (show?: boolean) => {
+      previewContainer.style.display = show ? "block" : "none";
+      localStorage.setItem(SHOW_PREVIEW_KEY, String(show));
+    };
+
+    // 初回処理
+    updatePreviewContainer(showPreview);
+    // トグルボタン
+    toggle.addEventListener("click", () => {
+      showPreview = !showPreview;
+      updatePreviewContainer(showPreview);
+    });
+    editor.addEventListener("compositionstart", async event => {
       isComposing = true;
     });
-    editor.addEventListener("compositionend", async (event) => {
+    editor.addEventListener("compositionend", async event => {
       const target = event.target as HTMLInputElement;
       if (!target) {
         return;
@@ -40,7 +78,7 @@ const main = async () => {
       isComposing = false;
       updatePreview(target.value);
     });
-    editor.addEventListener("input", async (event) => {
+    editor.addEventListener("input", async event => {
       const target = event.target as HTMLInputElement;
       if (!target) {
         return;
@@ -57,9 +95,10 @@ const main = async () => {
   // 前回終了時の状態をリストア
   const lastState = await compiler.getLastState();
   if (lastState) {
-    (editor as HTMLInputElement).value = lastState.raw;
-    (preview as HTMLTextAreaElement).innerHTML = lastState.html;
+    editor.value = lastState.raw;
+    preview.innerHTML = lastState.html;
   }
+  editor.focus();
 };
 
 main();
